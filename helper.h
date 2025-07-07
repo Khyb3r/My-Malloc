63ff1aad094a95b0478f68348fee9e8627d6353e
Khyb3r <157541714+Khyb3r@users.noreply.github.com>
2025-07-07 21:50:28 +0100

    Basic setup of data structures and malloc method

---
+++ helper.h
@@ -0,0 +1,17 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+// free list structure
+typedef struct _node_t {
+    int size;
+    struct _node_t *next;
+} node_t ;
+
+// header when allocating a block
+typedef struct {
+    int size;
+    int magic;
+} header_t ;
+
+
+#endif //HELPER_H
--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <sys/mman.h>
+#include "helper.h"
+
+void *malloc(size_t);
 
 int main(void) {
-    printf("Hello, World!\n");
+    // mmap() returns a pointer to a chunk of free space
+    node_t *head = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
+    head->size = 4096 - sizeof(node_t);
+    head->next = NULL;
     return 0;
+}
+
+// allocate memory
+void *malloc(size_t bytes_requested) {
+    // actual byte amount
+    size_t actual_memory_size = bytes_requested + sizeof(header_t);
+    return
 }
