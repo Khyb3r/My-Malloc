7079fb9a55e56ca628cb40990b0eadbaa1f37a6b
Khyb3r <157541714+Khyb3r@users.noreply.github.com>
2025-07-08 17:02:59 +0100

    Fixed pointer arithmetic issues with returning pointer

--- main.c
+++ main.c
@@ -8,11 +8,12 @@ void *my_malloc(size_t);
 void my_free(void *);
 
 #define HEAP_SIZE 4096
+#define MAGIC_NUMBER 12345
 
-// mmap() returns a pointer to a chunk of free space
-node_t *head = mmap(NULL, HEAP_SIZE, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
-
-int main(void) {
+node_t *head;
+int main(int argc, char*argv[]) {
+    // mmap() returns a pointer to a chunk of free space
+    head = mmap(NULL, HEAP_SIZE, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
     head->size = 4096 - sizeof(node_t);
     head->next = NULL;
 
@@ -22,9 +23,11 @@ int main(void) {
         fprintf(stderr, "Malloc error\n");
         exit(1);
     }
+    for (int i = 0; i < 2; i++) {
+        p_array[i] = 0;
+        printf("p_array[%d] = %d", i , p_array[i]);
+    }
     free(p_array);
-
-
     return 0;
 }
 
@@ -40,19 +43,23 @@ void *my_malloc(size_t bytes_requested) {
     while (p_node != NULL) {
         // first fit algorithm
         if (p_node->size >= actual_memory_size) {
+
             // create header for new memory block being allocated
             header_t *new_block = (header_t * ) p_node;
             new_block->size = (int) bytes_requested;
-
-            // Will figure out how to do the magic after
-            //new_block->magic = ;
+            new_block->magic = MAGIC_NUMBER;
+            assert(new_block->magic == MAGIC_NUMBER);
 
             // move head down
-            head += actual_memory_size;
+            uint8_t *p_temp = (uint8_t *) head + actual_memory_size;
+            head = (node_t *) p_temp;
             head->size = HEAP_SIZE - (bytes_requested + sizeof(header_t));
             head->next = NULL;
             p_node = NULL;
-            return new_block + sizeof(header_t);
+
+            uint8_t *p_temp_new_block = (uint8_t *) new_block + sizeof(header_t);
+            void *pointer = (void *) p_temp_new_block;
+            return pointer;
         }
         // move pointer along
         p_node = head->next;
@@ -61,6 +68,13 @@ void *my_malloc(size_t bytes_requested) {
 }
 
 void my_free(void *ptr) {
+    // free memory
     header_t *p_header = (header_t *) ptr - 1;
-    assert(p_header->magic);
+    assert(p_header->magic == MAGIC_NUMBER);
+
+    // need to coalesce back into the free list
+    node_t *p_node = (node_t *) p_header;
+    head->next = (node_t *) p_header;
+    p_node->size = p_header->size;
+    p_node->next = NULL;
 }
